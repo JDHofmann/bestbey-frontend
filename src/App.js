2f49bbe39aa094c1e324d5a6d7764ffc13db5d02
jdhofmann <justindhofmann@gmail.com>
2021-01-21 12:07:42 -0500

    added sass files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import './App.css';
-import Home from './containers/Home'
+// import './App.css';
+import './styles/app.css';
+import Home from './containers/Home';
 import NavBar from './containers/NavBar';
 import ConfirmationPage from './containers/ConfirmationPage'
 import {BrowserRouter as Router, Redirect, Route} from 'react-router-dom';
